e66bb3421716124183967d8a3e0a4e534be58f75
swisniewski974-coder <swisniewski974@gmail.com>
2026-09-24 08:30:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from discord.ext import commands
 
 # --- KONFIGURACJA ---
 # Wklej swój token w cudzysłowie poniżej:
-TOKEN = "TUTAJ_WKLEJ_SWÓJ_TOKEN_DISCORD"
+TOKEN = "TOKEN = os.getenv("TOKEN")
+"
 GIELDA_CHANNEL_ID = (  # Zmień na ID swojego kanału giełdowego (musi być samymi cyframi!)
     123456789012345678
 )
